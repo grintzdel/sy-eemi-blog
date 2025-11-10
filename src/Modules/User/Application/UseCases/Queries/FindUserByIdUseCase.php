e88be848b33f2d14fee41d1bd10c90702ee6e0ca
maoudin <mathis.oudin42@gmail.com>
2025-11-10 15:53:23 +0100

    refactor: rename file

--- src/Modules/User/Application/UseCases/Queries/FindUserByIdUseCase.php
+++ src/Modules/User/Application/UseCases/Queries/FindUserByIdUseCase.php
@@ -8,7 +8,7 @@ use App\Modules\User\Domain\Entities\UserEntity;
 use App\Modules\User\Domain\Repositories\IUserRepository;
 use App\Modules\User\Domain\ValueObjects\UserId;
 
-final readonly class FindUserById
+final readonly class FindUserByIdUseCase
 {
     public function __construct(
         private IUserRepository $userRepository,
