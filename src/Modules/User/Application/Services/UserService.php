8bb6ef386f34952fb1001360393235e22a47e783
maoudin <mathis.oudin42@gmail.com>
2025-11-10 15:56:22 +0100

    feat: update service

--- src/Modules/User/Application/Services/UserService.php
+++ src/Modules/User/Application/Services/UserService.php
@@ -4,7 +4,31 @@ declare(strict_types=1);
 
 namespace App\Modules\User\Application\Services;
 
+use App\Modules\User\Application\Commands\CreateUserCommand;
+use App\Modules\User\Application\UseCases\Commands\CreateUserUseCase;
+use App\Modules\User\Application\UseCases\Queries\FindUserByIdUseCase;
+use App\Modules\User\Domain\Entities\UserEntity;
+
 final readonly class UserService
 {
-    public function __construct() {}
+    public function __construct(
+        private CreateUserUseCase   $createUserUseCase,
+        private FindUserByIdUseCase $findUserByIdUseCase,
+    ) {}
+
+    /*
+     * Commands
+     */
+    public function create(CreateUserCommand $user): UserEntity
+    {
+        return $this->createUserUseCase->execute($user);
+    }
+
+    /*
+     * Queries
+     */
+    public function findById(string $id): UserEntity
+    {
+        return $this->findUserByIdUseCase->execute($id);
+    }
 }
