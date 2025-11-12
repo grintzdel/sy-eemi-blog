3eea879530468c9129c3e68e5eacb5108c8d50bc
maoudin <mathis.oudin42@gmail.com>
2025-11-12 19:48:31 +0100

    fix: fix home controller with Article view model

--- src/Modules/Shared/Presentation/Controllers/HomeController.php
+++ src/Modules/Shared/Presentation/Controllers/HomeController.php
@@ -4,20 +4,25 @@ declare(strict_types=1);
 
 namespace App\Modules\Shared\Presentation\Controllers;
 
-use App\Modules\Article\Application\Services\ArticleService;
+use App\Modules\Article\Infrastructure\Doctrine\Repositories\DoctrineArticleRepository;
+use App\Modules\Article\Presentation\ViewModels\ArticleViewModel;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AppController
 {
     public function __construct(
-        private readonly ArticleService $articleService
+        private readonly DoctrineArticleRepository $articleRepository,
     ) {}
 
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $articles = $this->articleService->findAll();
+        $doctrineArticles = $this->articleRepository->findAllDoctrineEntities();
+        $articles = array_map(
+            static fn($article) => ArticleViewModel::fromDoctrineEntity($article),
+            $doctrineArticles
+        );
 
         return $this->render('home/index.html.twig', [
             'articles' => $articles,
