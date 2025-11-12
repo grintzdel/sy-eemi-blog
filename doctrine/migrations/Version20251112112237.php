cd1b83c69b8a70a1c5a4715d2e52f68a9f351ece
maoudin <mathis.oudin42@gmail.com>
2025-11-12 13:29:03 +0100

    feat: author username migration

--- doctrine/migrations/Version20251112112237.php
+++ doctrine/migrations/Version20251112112237.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251112090457 extends AbstractMigration
+final class Version20251112112237 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,18 @@ final class Version20251112090457 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE articles (id VARCHAR(36) NOT NULL, heading VARCHAR(255) NOT NULL, subheading VARCHAR(255) NOT NULL, content TEXT NOT NULL, author VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE TABLE articles (id VARCHAR(36) NOT NULL, heading VARCHAR(255) NOT NULL, subheading VARCHAR(255) NOT NULL, content TEXT NOT NULL, cover_image VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, author_id VARCHAR(36) NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE INDEX IDX_BFDD3168F675F31B ON articles (author_id)');
         $this->addSql('CREATE TABLE users (id VARCHAR(36) NOT NULL, username VARCHAR(20) NOT NULL, email VARCHAR(255) NOT NULL, age INT NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY (id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9F85E0677 ON users (username)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
+        $this->addSql('ALTER TABLE articles ADD CONSTRAINT FK_BFDD3168F675F31B FOREIGN KEY (author_id) REFERENCES users (id) NOT DEFERRABLE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE articles DROP CONSTRAINT FK_BFDD3168F675F31B');
         $this->addSql('DROP TABLE articles');
         $this->addSql('DROP TABLE users');
     }
